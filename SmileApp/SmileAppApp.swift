f078b6b8e7e75ec930465fbaabdacc9fd9a0a8b0
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-01 21:16:23 +0900

    Initial Commit

---
+++ SmileApp/SmileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileAppApp.swift
+//  SmileApp
+//
+//  Created by 深根幹貴 on 2021/06/01.
+//
+
+import SwiftUI
+
+@main
+struct SmileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
